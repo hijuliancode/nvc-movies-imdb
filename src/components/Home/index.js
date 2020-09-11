082bbb0d9051be15593f226d45f23125911cb55b
hijuliancode <hijuliansosa@protonmail.com>
2020-09-10 23:23:57 -0500

    Home : Search bar

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { Input } from 'antd'
+
 // Components
+const { Search } = Input;
 
 // Styles
 const HomeElm = styled.div`
+  padding: ${props => props.theme.baseSize * 6}px;
+  width: 100%;
+`
+const SearchWrapper = styled.div`
+  margin-bottom: ${props => props.theme.baseSize * 6}px;
 `
 
 const HomeComponent = () => (
   <HomeElm>
-    Home Works!
+    <SearchWrapper>
+      <Search
+        placeholder="input search text"
+        enterButton="Search"
+        size="large"
+        onSearch={value => console.log(value)}
+      />
+    </SearchWrapper>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, minima hic sint cumque in deleniti dignissimos nam inventore eius, quia facilis distinctio natus, commodi similique voluptates. Officiis maxime dolore quidem.
   </HomeElm>
 )
 
