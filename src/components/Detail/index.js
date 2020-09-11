183a9d919254b2878b510e4bc6e199a419699998
hijuliancode <hijuliansosa@protonmail.com>
2020-09-10 23:00:17 -0500

    Home component

---
+++ src/components/Detail/index.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import styled from 'styled-components'
+
+// Components
+
+// Styles
+const DetailElm = styled.div``
+
+const DetailComponent = () => {
+  <HomeElm>
+    DetailComponent Works!
+  </HomeElm>
+}
+
+export default DetailComponent
